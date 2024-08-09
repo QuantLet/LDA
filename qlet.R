c7e660d3e8d6489ae62e3de0c7bb07f7afeeee06
Julian <julian.winkel@hu-berlin.de>
2024-08-09 21:45:03 +0200

    updated config and unique row behavior

--- qlet.R
+++ qlet.R
@@ -17,56 +17,46 @@ library(readtext)
 # Machine Learning
 # Cryptocurrency
 
-wd = '/Users/Julian/src/LDA'
+### Config
+wd = paste0(Sys.getenv('HOME'), '/src/LDA') # for macOS
 setwd(wd)
 
-# Static
-qlet_data = 'data/Quantlet_Database.json' #Quantlet json
+# Choose whether to make the topic assignment dependent on the specified keywords or the relevant_text variable. 
+# Default version works with KEYWORDS
+text_field_var = 'keywords' 
+#text_field_var = 'relevant_text' 
 
+# Static
+qlet_data = 'data/Quantlet_Database.json' # Quantlet json
+out_file = 'qlet_topics_assigned_v2.csv' # Output file name
 
 
 # Define dictionary
-dict_topic = dictionary(list(datascience=c("data mining", "analytics", "cluster", "Monte Carlo", "SQL", "predict*", "communication", "data clean*", "data science", "data management", "NLP", "natural language processing", "data visualization", "business intelligence", "BI", "random number", "stationary", "regression"), 
-                             fintech=c("api", "regulatory", "regulatory technology", "regtech", "KYC", "AML", "fintech", "fin tech", "3D secure", "Chargeback", "Crowdfunding"),
+dict_topic = dictionary(list(datascience=c("data mining", "analytics", "cluster", "Monte Carlo", "SQL", "predict*", "communication", "data clean*", "data science", "data management", "NLP", "natural language processing", "data visualization", "business intelligence", "BI", "random number", "stationary", "regression", "correlation", "dependence", "multivariate*", "visualization"), 
+                             fintech=c("api", "regulatory", "regulatory technology", "regtech", "KYC", "AML", "fintech", "fin tech", "3D secure", "Chargeback", "Crowdfunding", "financial", "*financ*"),
                              blockchain=c("blockchain", "block chain", "block*", "coin", "consensus", "POW", "POS", "Proof of Work", "Proof of Stake", "Contract", "Smart Contract", "Cryptography", "Decentralization", "Fork", "Node", "Hash", "Mining", "Byzantine"),
                              explainableai=c('Explainable AI', "Explain*","XAI", "Transparency", "Audit*", "Transparent", "Concept*", "Concept Explanation", "Interpretability", "Econometric*"),
-                             machinelearning=c("Machine Learning", "ML", "AI", "*Boost", "Tune", "Neural Network", "ARIMA", "NN", "Neural Network", "Deep Learning", "predict*", "LSTM","Supervise", "Classification", "Classifier", "Learner", "Robot", "Overfit*", "SVM", "Support Vector Machine", "Stationarity", "*starionar*", "Gradient Boost", "Boost", "Logistic Regression", "regression", "LDA", "Latent", "Topic Model*"),
-                             cryptocurrency=c("crypto*", "Bitcoin", "BTC", "Ether*", "ETH", "Crypto Exchange", "Digital Currency", "Digicash", "Digital Wallet", "Wallet", "FIAT", "NFT", "Non-fundigble Token", "Stable Coin", "Stablecoin", "Tether", "USDT")
+                             machinelearning=c("Machine Learning", "ML", "AI", "XGBoost", "XG", "Tune", "Neural Network", "ARIMA", "NN", "Neural Network", "Deep Learning", "predict*", "forecast*", "LSTM","Supervise", "Classification", "Classifier", "Learner", "Robot", "Overfit*", "SVM", "Support Vector Machine", "Stationarity", "*starionar*", "Gradient Boost", "Boost", "Logistic Regression", "regression", "LDA", "Latent", "Topic Model*", "GAN", "Nadaraya Watson", "FRM"),
+                             cryptocurrency=c("crypto*", "Bitcoin", "BTC", "Ether*", "ETH", "Crypto Exchange", "CRIX", "DAI", "Deribit", "Digital Currency", "Digicash", "Digital Wallet", "Wallet", "FIAT", "NFT", "Non-fundigble Token", "Stable Coin", "Stablecoin", "Smart Contract*","Tether", "USDT")
 ))
 print(dict_topic)
 
 
-qj = jsonlite::fromJSON(qlet_data)
-#qj_unique = qj[qj$repo_name == unique(qj$repo_name),]
+qj_unique = jsonlite::fromJSON(qlet_data)
 
-print("attention here")
-# Instead of omitting information, we could also just merge the information that we have from the duplicate entries and afterwards select unique relevant_text statements
-# So merge keywords and description of all rows where the identifier "repo_name" is the same
-# pretty good idea actually.
-# The other option would be to go line-by-line and only consider the relevant text of each row for itself. 
-
-for(curr_repo in unique(qj$repo_name)[1:30]){
-  print(curr_repo)
-  print(qj[qj$repo_name == curr_repo,'keywords'])
-  print('---')
-}
-
-# quantlet names are not unique!
-qj_unique = qj[!duplicated(qj[,'repo_name']),]
+# quantlet names are not unique! this is why we work with the defined 'docid' below
+qj_unique['docid'] = paste(qj_unique$repo_name, '-', qj_unique$name_of_quantlet, '-', rownames(qj_unique))
 
 # Construct Corpus from Quantlet JSON
 qj_unique$relevant_text = paste(qj_unique$description, qj_unique$keywords)
-q_corpus = corpus(qj_unique, text_field = 'keywords', docid_field = 'repo_name') #switch back to relevant_text!!
+q_corpus = corpus(qj_unique, text_field = text_field_var, docid_field = 'docid') #switch back to relevant_text!!
 summary(q_corpus, n = 5)
 ndoc(q_corpus)
 
 toks_news_raw <- tokens(q_corpus, remove_punct = TRUE, remove_numbers = TRUE, remove_symbol = TRUE)
 toks_news <- tokens_remove(toks_news_raw, pattern = c(stopwords("en"), "*-time", "updated-*", "gmt", "bst"))
 
-
 ##
-
-
 dfmat_news <- dfm(toks_news) %>% 
   dfm_trim(min_termfreq = 0.8, termfreq_type = "quantile",
            max_docfreq = 0.1, docfreq_type = "prop")
@@ -82,12 +72,8 @@ dfmat_news$topic2 <- topics(tmod_slda)
 # cross-table of the topic frequency
 table(dfmat_news$topic2)
 
-
-
 # Tests
-
 un_keywords = unique(qj$keywords)
-
 
 # Inspect outcome for individual topics
 tops = topics(tmod_slda)
@@ -95,9 +81,6 @@ tops[tops %in% c('cryptocurrency')]
 
 # Save Output
 qj_unique$assigned_topic = tops
-write.csv(qj_unique, file = 'qlet_topics_assigned.csv')
+write.csv(qj_unique, file = out_file)
 
 
-
-# For Comparison
-#old_results = qj_unique
